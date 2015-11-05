8e4021af7f1fe52de62959eae8ec78c533dae507
spaar <spaar.modding@gmail.com>
2015-11-05 19:55:56 +0100

    Initial commit

---
+++ FreeCamera/Mod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using spaar.ModLoader;
+using UnityEngine;
+
+namespace spaar.Mods.FreeCamera
+{
+
+  // If you need documentation about any of these values or the mod loader
+  // in general, take a look at https://spaar.github.io/besiege-modloader.
+
+  public class FreeCameraMod : spaar.ModLoader.Mod
+  {
+    public override string Name { get; } = "freeCamera";
+    public override string DisplayName { get; } = "Free Camera";
+    public override string Author { get; } = "spaar";
+    public override Version Version { get; } = new Version(1, 0, 0);
+
+    public override string VersionExtra { get; } = "";
+
+    public override bool CanBeUnloaded { get; } = false;
+    public override bool Preload { get; } = false;
+
+
+    public override void OnLoad()
+    {
+
+    }
+
+    public override void OnUnload()
+    {
+
+    }
+  }
+}
